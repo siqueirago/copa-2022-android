b020e729ea72811f393499cfda660a66f3c39cd9
Julio Siqueira <siqueiragomes123@gmail.com>
2024-02-08 11:51:56 -0300

    Concluindo a desafio App Copa 2022- Curso Formação Android Developer

--- notification-scheduler/src/main/kotlin/me/dio/copa/catar/notification/scheduler/extensions/NotificationExtensions.kt
+++ notification-scheduler/src/main/kotlin/me/dio/copa/catar/notification/scheduler/extensions/NotificationExtensions.kt
@@ -1,10 +1,13 @@
 package me.dio.copa.catar.notification.scheduler.extensions
 
+import android.Manifest
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import me.dio.copa.catar.notification.scheduler.R
@@ -15,8 +18,23 @@ private const val NOTIFICATION_INTENT_REQUEST_CODE = 0
 
 fun Context.showNotification(title: String, content: String) {
     createNotificationChannel()
+
     val notification = getNotification(title, content)
 
+    if (ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.POST_NOTIFICATIONS
+        ) != PackageManager.PERMISSION_GRANTED
+    ) {
+        // TODO: Consider calling
+        //    ActivityCompat#requestPermissions
+        // here to request the missing permissions, and then overriding
+        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+        //                                          int[] grantResults)
+        // to handle the case where the user grants the permission. See the documentation
+        // for ActivityCompat#requestPermissions for more details.
+        return
+    }
     NotificationManagerCompat
         .from(this)
         .notify(content.hashCode(), notification)
@@ -47,5 +65,6 @@ private fun Context.getOpenAppPendingIntent() = PendingIntent.getActivity(
     this,
     NOTIFICATION_INTENT_REQUEST_CODE,
     packageManager.getLaunchIntentForPackage(packageName),
-    PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT,
+    PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
 )
+
